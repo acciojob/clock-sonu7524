084a08574ca83e1beb213b40788c40292831b90a
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-04-28 14:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function getTimer(){
+    const time = new Date();
+    let dayString = `${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
+    let timeString = "";
+    let hr = `${time.getHours()}`;
+    if(hr >= 12){
+        if(hr===12){
+            timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} PM`;
+        }
+        else{
+            hr -= 12;
+            timeString = `${hr}:${time.getMinutes()}:${time.getSeconds()} PM`;
+        }
+    }
+    else{
+        timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} AM`;
+    }
+
+    return dayString+" "+timeString;
+}
+
+const p = document.getElementById("timer");
+
+function timer(){
+    p.innerText = getTimer();
+}
+
+setInterval(timer,1000);
